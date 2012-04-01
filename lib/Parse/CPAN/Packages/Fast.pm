828e3b0f94b25a7c40711aa6f9dbb0c116204904
Slaven Rezic <slaven@rezic.de>
2012-04-01 11:01:27 +0200

    * 0.05_51

--- lib/Parse/CPAN/Packages/Fast.pm
+++ lib/Parse/CPAN/Packages/Fast.pm
@@ -22,7 +22,7 @@ use CPAN::DistnameInfo ();
 {
     package Parse::CPAN::Packages::Fast;
 
-    our $VERSION = '0.05_50';
+    our $VERSION = '0.05_51';
     $VERSION =~ s{_}{};
 
     use PerlIO::gzip;
