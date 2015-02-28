863887bbc7a4afb4097377029088d0860b17460d
jeffblack360 <jeffblack360@gmail.com>
2015-02-27 18:19:48 -0600

    Test with supplied package file

--- t/TestUtil.pm
+++ t/TestUtil.pm
@@ -4,6 +4,7 @@ package TestUtil;
 
 use strict;
 use Exporter 'import';
+use Cwd;
 
 our @EXPORT = qw(my_default_packages_file);
 
@@ -12,7 +13,7 @@ sub my_default_packages_file {
 	return Parse::CPAN::Packages::Fast->_default_packages_file_interactive;
     }
 
-    my $packages_file = eval { Parse::CPAN::Packages::Fast->_default_packages_file_batch };
+    my $packages_file = getcwd() . '/t/02packages.details.txt.gz';
     return undef if $packages_file && (!-r $packages_file || -z $packages_file);
     $packages_file;
 }
